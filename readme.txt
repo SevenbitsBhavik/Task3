835f0a92dedc5d023726bd8bc75051289380a974
Bhavik <bhavik@sevenbits.in>
2022-06-10 12:28:51 +0530

    c3

---
+++ readme.txt
@@ -0,0 +1,2 @@
+install all the dependencies like web3 mysql 
+add your infuralink, senderData(address & private_key), receiver_address.
--- sender.js
+++ sender.js
@@ -2,19 +2,17 @@ const Web3 = require("web3");
 const con = require("./databse");
 const {infuraLink} = require("./config/infuraLink")
 const web3 = new Web3(new Web3.providers.HttpProvider(infuraLink))
-const EthereumTx = require('ethereumjs-tx').Transaction;
-const {senderData} = require('./config/sender_data')
+// const EthereumTx = require('ethereumjs-tx').Transaction;
+const {senderData} = require('./config/sender_data');
 const senderAddress = senderData.senderAddress
 const privateKey = senderData.privateKey
-let recAddress = "0x0dc7f556d03882527dd92371c74b3739c5553ba2"
-// recAddress = "0x1a7a49dd847521daef30ea3de3c1f74771e5c875"
 
 let status= " ";
 let process = 0;
 
 
 async function sendEthereum(senderAdddress,privateKey, recAddress, amount){
-    var nonce = await web3.eth.getTransactionCount(senderAddress);
+    var nonce = await web3.eth.getTransactionCount(senderAddress) + 1;
     console.log(nonce);
     web3.eth.getBalance(senderAddress,async(err,result) => {
         if(err){
@@ -35,10 +33,7 @@ async function sign_transaction(senderAddress ,privateKey, recAddress, amount,no
         if(err){
             return err;
         }
-        // console.log(result[0].status)
-        console.log(result.length)
         if(result.length<=0){
-            nonce = nonce +1;
             let status = "pending";
             const createTransaction = await web3.eth.accounts.signTransaction(
                 {
@@ -46,14 +41,14 @@ async function sign_transaction(senderAddress ,privateKey, recAddress, amount,no
                    to: recAddress,
                    value: web3.utils.toWei(amount.toString(), 'ether'),
                    gas: '21000',
+                //    nonce: nonce
                 },
                 privateKey
              );
              status = "pending";
-             process = process+1;
-             con.query("Insert into transaction(id, from_add, to_add, amount, status, process, transactio_hash, raw_transaction, timestamp) values(?,?,?,?,?,?,?,?,?)",
+             console.log(createTransaction);
+             con.query("Insert into transaction(id, from_add, to_add, amount, status, process, transaction_hash, raw_transaction, timestamp) values(?,?,?,?,?,?,?,?,?)",
                 [nonce, senderAddress, recAddress, amount, status, process,createTransaction.transactionHash,createTransaction.rawTransaction,Math.floor(Date.now() / 1000)]);
-            console.log(createTransaction);
 
         }
         else{
@@ -69,34 +64,55 @@ async function sign_transaction(senderAddress ,privateKey, recAddress, amount,no
                    to: recAddress,
                    value: web3.utils.toWei(amount.toString(), 'ether'),
                    gas: '21000',
+                //    nonce: nonce
                 },
                 privateKey
              );
              status = "pending";
-             process = process+1;
-             con.query("Insert into transaction(id, from_add, to_add, amount, status, process, transactio_hash, raw_transaction, timestamp) values(?,?,?,?,?,?,?,?,?)",
+             con.query("Insert into transaction(id, from_add, to_add, amount, status, process, transaction_hash, raw_transaction, timestamp) values(?,?,?,?,?,?,?,?,?)",
                 [nonce, senderAddress, recAddress, amount, status, process,createTransaction.transactionHash,createTransaction.rawTransaction,Math.floor(Date.now() / 1000)]);
             console.log(createTransaction);
+            // createReceipt = await web3.eth.sendSignedTransaction(createTransaction.rawTransaction)
              return createTransaction;
         }
     });
 }
 
 async function deploy_transaction(){
-    con.query("Select * from transaction where status = 'pending'",async function(err, result){
+    con.query("Update transaction set process = 1 where process = 0")
+    con.query("Select * from transaction where status = 'pending' || status = 'failed'",async function(err, result){
         if(err){
             return err;
         }
-        for(let i=0;i<result.length;i++){
-            await web3.eth.getTransactionCount("0x6343D7675ef45d12ED09e66ec45CD12BC5f6423d").then(console.log)
-            web3.eth.sendSignedTransaction(result[i].raw_transaction);
-            con.query("Update transaction set timestamp = ? where status = 'pending'",[Math.floor(Date.now() / 1000)]);
-            con.query("Update transaction set status = 'successfull' where id = "+result[i].id);
-            web3.setTransactionCount
-            console.log("Transaction successful with hash: " + result[i].raw_transaction)
+        if(result.length<=0){
+            return
+        }
+        else{
+            console.log(result)
+            for(let i=0;i<result.length;i++){
+                let createReceipt
+                try {
+                    createReceipt = await web3.eth.sendSignedTransaction(result[i].raw_transaction)
+                    console.log(createReceipt.transactionHash)
+                    await con.query("Update transaction set timestamp = ? , status = 'successfull' , process = 2 where status = 'pending'",[Math.floor(Date.now() / 1000)]);
+                    await console.log("Transaction Successfull with transaction_hash:"+result[i].transaction_hash)
+                } catch (error) {
+                    console.log(" " +  error)
+                    con.query("Update transaction set timestamp = ? , status = 'failed' , process = 0 where id = ?"
+                        ,[Math.floor(Date.now() / 1000),result[i].id]);
+                 
+                    continue
+                }
+            }
+            
         }
     })
 }
+
+let recAddress = "0x0dc7f556d03882527dd92371c74b3739c5553ba2"
+// recAddress = "0x1a7a49dd847521daef30ea3de3c1f74771e5c875"
+
+
 
 deploy_transaction()
 sendEthereum(senderAddress,privateKey,recAddress,00)
